ba21df2e24b494a905f2c3ea7d6e545ddf84f7f2
mereus-art <peresmatheus147@gmail.com>
2021-11-23 21:46:18 -0300

    COMMIT 23.11

--- src/trabalho/controller/new-trabalho.js
+++ src/trabalho/controller/new-trabalho.js
@@ -4,10 +4,17 @@ $(document).ready(function() {
 
         $('.modal-title').empty()
         $('.modal-body').empty()
+
         $('.modal-title').append('Adicionar novo trabalho')
-        $('.modal-body').load('src/trabalho/view/form-trabalho.html', function() {})
+
+        $('.modal-body').load('src/trabalho/view/form-trabalho.html', function() {
+
+        })
+
         $('.btn-save').show()
+
         $('.btn-save').attr('data-operation', 'insert')
+
         $('#modal-trabalho').modal('show')
     })
 })
